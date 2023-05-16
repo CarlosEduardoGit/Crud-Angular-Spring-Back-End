b1e82ecfccf6a75c2427cc15708531edd90b614c
CarlosEduardoGit <cadudjn1@gmail.com>
2023-05-16 00:06:11 -0300

    Listar Cursos (API HTTP GET)

---
+++ src/main/java/com/carlos/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.carlos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carlos.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long>{
+
+}
