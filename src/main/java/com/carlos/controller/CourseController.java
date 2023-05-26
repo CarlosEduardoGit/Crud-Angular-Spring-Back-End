27953d35c801f7e2d431861f455928408f59abc7
CarlosEduardoGit <cadudjn1@gmail.com>
2023-05-26 09:58:15 -0300

    Formulário: API Spring: Criar Curso (HTTP POST)

--- src/main/java/com/carlos/controller/CourseController.java
+++ src/main/java/com/carlos/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.carlos.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carlos.model.Course;
@@ -23,4 +28,9 @@ public class CourseController {
 		return courseRepository.findAll();
 	}
 
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Course create(@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
 }
