509ff447499017b5a5710a4e47ae8aadb38de0fd
CarlosEduardoGit <cadudjn1@gmail.com>
2023-05-29 21:16:37 -0300

    Atualizar Curso: API Spring HTTP PUT

--- src/main/java/com/carlos/controller/CourseController.java
+++ src/main/java/com/carlos/controller/CourseController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,14 +27,14 @@ public class CourseController {
 	private final CourseRepository courseRepository;
 	
 	@GetMapping
-	public List<Course> list() {
+	public @ResponseBody List<Course> list() {
 		return courseRepository.findAll();
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Course> findById(@PathVariable Long id) {
 		return courseRepository.findById(id)
-				.map(record -> ResponseEntity.ok().body(record))
+				.map(recordFound -> ResponseEntity.ok().body(recordFound))
 				.orElse(ResponseEntity.notFound().build());
 	}
 
@@ -40,5 +42,18 @@ public class CourseController {
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Course create(@RequestBody Course course) {
 		return courseRepository.save(course);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+		return courseRepository.findById(id)
+				.map(recordFound -> {
+					recordFound.setName(course.getName());
+					recordFound.setName(course.getCategory());
+					Course updated = courseRepository.save(recordFound);
+					return ResponseEntity.ok().body(updated);
+				})
+				.orElse(ResponseEntity.notFound().build());
+		
 	}
 }
