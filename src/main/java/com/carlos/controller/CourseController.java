c348a8ba7419782fbadd282caff96ef66a5fdc5c
CarlosEduardoGit <cadudjn1@gmail.com>
2023-06-05 21:29:29 -0300

    Padrão Mapper

--- src/main/java/com/carlos/controller/CourseController.java
+++ src/main/java/com/carlos/controller/CourseController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.carlos.dto.CourseDTO;
 import com.carlos.model.Course;
 
 import com.carlos.service.CourseService;
@@ -38,23 +39,23 @@ public class CourseController {
 	}
 	
 	@GetMapping
-	public @ResponseBody List<Course> list() {
+	public @ResponseBody List<CourseDTO> list() {
 		return courseService.list();
 	}
 	
 	@GetMapping("/{id}")
-	public Course findById(@PathVariable @NotNull @Positive Long id) {
+	public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
 		return courseService.findById(id);
 	}
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public Course create(@RequestBody @Valid Course course) {
+	public CourseDTO create(@RequestBody @Valid CourseDTO course) {
 		return courseService.create(course);
 	}
 	
 	@PutMapping("/{id}")
-	public Course update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
+	public CourseDTO update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid CourseDTO course) {
 		return courseService.update(id, course);
 				
 	}
--- src/main/java/com/carlos/service/CourseService.java
+++ src/main/java/com/carlos/service/CourseService.java
@@ -1,14 +1,17 @@
 package com.carlos.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.carlos.dto.CourseDTO;
+import com.carlos.dto.mapper.CourseMapper;
 import com.carlos.exception.RecordNotFoundException;
 import com.carlos.model.Course;
 import com.carlos.repository.CourseRepository;
@@ -22,35 +25,41 @@ import jakarta.validation.constraints.Positive;
 public class CourseService {
 
 	private final CourseRepository courseRepository;
+	private final CourseMapper courseMapper;
 	
-	public CourseService(CourseRepository courseRepository) {
+	public CourseService(CourseRepository courseRepository, CourseMapper courseMapper) {
 		this.courseRepository = courseRepository;
+		this.courseMapper = courseMapper;
 	}
 	
-	public List<Course> list() {
-		return courseRepository.findAll();
+	public List<CourseDTO> list() {
+		return courseRepository.findAll()
+				.stream()
+				.map(courseMapper::toDTO)
+				.collect(Collectors.toList());
 	}
 	
-	public Course findById(@PathVariable @NotNull @Positive Long id) {
-		return courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
+	public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
+		return courseRepository.findById(id).map(courseMapper::toDTO)
+				.orElseThrow(() -> new RecordNotFoundException(id));
 	}
 	
-	public Course create(@Valid Course course) {
-		return courseRepository.save(course);
+	public CourseDTO create(@Valid @NotNull CourseDTO course) {
+		return courseMapper.toDTO(courseRepository.save(courseMapper.toEntity(course)));
 	}
 	
-	public Course update(@NotNull @Positive Long id, @RequestBody @Valid Course course) {
+	public CourseDTO update(@NotNull @Positive Long id, @Valid @NotNull CourseDTO course) {
 		return courseRepository.findById(id)
 				.map(recordFound -> {
-					recordFound.setName(course.getName());
-					recordFound.setName(course.getCategory());
-					return courseRepository.save(recordFound);
-					
+					recordFound.setName(course.name());
+					recordFound.setName(course.category());
+					return courseMapper.toDTO(courseRepository.save(recordFound));		
 				}).orElseThrow(() -> new RecordNotFoundException(id));
 	}
 	
 	public void delete(@PathVariable @NotNull @Positive Long id) {
 		
-		courseRepository.delete(courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id)));		
+		courseRepository.delete(courseRepository.findById(id)
+				.orElseThrow(() -> new RecordNotFoundException(id)));		
 	}
 }
